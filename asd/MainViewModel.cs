2d56352c2fc19db9529339a739e9a1260a90c37d
Fabio Sulser <fabio.sulser@gmail.com>
2019-04-02 08:53:57 +0200

    initial commit

---
+++ asd/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace asd
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Person> People { get; set; }
+     
+        public MainViewModel()
+        {
+
+            People = new ObservableCollection<Person>
+            {
+                new Person
+                {
+                    Name = "Name1",
+                    Surname = "Surname1",
+                    Age = 50
+                },
+                new Person
+                {
+                    Name = "Name2",
+                    Surname = "Surname2",
+                    Age = 23
+                },
+                new Person
+                {
+                    Name = "Name3",
+                    Surname = "Surname3",
+                    Age = 44
+                },
+            };
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+
+
+}
